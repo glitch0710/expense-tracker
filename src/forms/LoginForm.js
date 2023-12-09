bdb7b82ae6470aae1091a340a25ae9752143c8ce
Rhogiel Andoy <theglitch0710@gmail.com>
2023-12-09 21:31:52 +0800

    #12 integrate formik

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -2,13 +2,12 @@ import { View, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { StackActions, useNavigation } from "@react-navigation/native";
 import { useTheme, Text, Button, TextInput } from "react-native-paper";
+import { Formik } from "formik";
 
 const LoginForm = ({ navigation }) => {
   const theme = useTheme();
   const navigations = useNavigation();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [hidePass, setHidePass] = useState(true);
 
   const registerHandler = () => {
@@ -19,8 +18,8 @@ const LoginForm = ({ navigation }) => {
     navigation.navigate("ResetPassword");
   };
 
-  const userLoginHandler = () => {
-    // navigation.dispatch(StackActions.replace("Dashboard"));
+  const userLoginHandler = (values) => {
+    console.debug(values);
     navigations.reset({
       index: 0,
       routes: [{ name: "Dashboard" }],
@@ -31,63 +30,84 @@ const LoginForm = ({ navigation }) => {
     navigation.navigate("LandingScreen");
   };
 
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
   return (
-    <View>
-      <TextInput
-        outlineColor={theme.colors.secondary}
-        style={styles.textInput}
-        label="Username"
-        mode="outlined"
-        value={username}
-        onChangeText={(username) => setUsername(username)}
-      />
-      <TextInput
-        outlineColor={theme.colors.secondary}
-        style={{ ...styles.textInput, marginTop: 10 }}
-        label="Password"
-        mode="outlined"
-        value={password}
-        secureTextEntry={hidePass ? true : false}
-        right={
-          <TextInput.Icon icon="eye" onPress={() => setHidePass(!hidePass)} />
-        }
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Button
-        style={styles.buttonStyle}
-        buttonColor="#DBD92E"
-        icon="login"
-        mode="contained"
-        onPress={() => userLoginHandler()}
-      >
-        Login
-      </Button>
-      <Button
-        style={{ ...styles.buttonStyle, marginTop: 15 }}
-        buttonColor="#2E98DB"
-        icon="arrow-left-bold-box-outline"
-        mode="contained"
-        onPress={() => landingHandler()}
-      >
-        Back
-      </Button>
-      <Button
-        style={{ ...styles.buttonStyle, marginTop: 15 }}
-        icon="account-multiple-plus"
-        mode="outline"
-        onPress={() => registerHandler()}
-      >
-        Don't have an account yet?
-      </Button>
-      <Button
-        style={{ ...styles.buttonStyle, marginTop: 0 }}
-        icon="lock-reset"
-        mode="outline"
-        onPress={() => resetPasswordHandler()}
-      >
-        Forgot Password
-      </Button>
-    </View>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => {
+        userLoginHandler(values);
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+        <View>
+          <TextInput
+            outlineColor={theme.colors.secondary}
+            style={styles.textInput}
+            label="Username"
+            mode="outlined"
+            value={values.username}
+            onChangeText={handleChange("username")}
+            onBlur={handleBlur("username")}
+            error={values.username}
+          />
+          <TextInput
+            outlineColor={theme.colors.secondary}
+            style={{ ...styles.textInput, marginTop: 10 }}
+            label="Password"
+            mode="outlined"
+            value={values.password}
+            onBlur={handleBlur("password")}
+            error={values.password}
+            secureTextEntry={hidePass ? true : false}
+            right={
+              <TextInput.Icon
+                icon="eye"
+                onPress={() => setHidePass(!hidePass)}
+              />
+            }
+            onChangeText={handleChange("password")}
+          />
+          <Button
+            style={styles.buttonStyle}
+            buttonColor="#DBD92E"
+            icon="login"
+            mode="contained"
+            onPress={handleSubmit}
+          >
+            Login
+          </Button>
+          <Button
+            style={{ ...styles.buttonStyle, marginTop: 15 }}
+            buttonColor="#2E98DB"
+            icon="arrow-left-bold-box-outline"
+            mode="contained"
+            onPress={() => landingHandler()}
+          >
+            Back
+          </Button>
+          <Button
+            style={{ ...styles.buttonStyle, marginTop: 15 }}
+            icon="account-multiple-plus"
+            mode="outline"
+            onPress={() => registerHandler()}
+          >
+            Don't have an account yet?
+          </Button>
+          <Button
+            style={{ ...styles.buttonStyle, marginTop: 0 }}
+            icon="lock-reset"
+            mode="outline"
+            onPress={() => resetPasswordHandler()}
+          >
+            Forgot Password
+          </Button>
+        </View>
+      )}
+    </Formik>
   );
 };
 
--- src/forms/RegisterForm.js
+++ src/forms/RegisterForm.js
@@ -1,13 +1,11 @@
 import { View, StyleSheet } from "react-native";
 import React, { useState } from "react";
+import { Formik } from "formik";
 import { useTheme, Text, Button, TextInput } from "react-native-paper";
 
 const RegisterForm = ({ navigation }) => {
   const theme = useTheme();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [hidePass, setHidePass] = useState(true);
   const [hideConfirmPass, setHideConfirmPass] = useState(true);
 
@@ -15,61 +13,80 @@ const RegisterForm = ({ navigation }) => {
     navigation.navigate("LoginScreen");
   };
 
+  const initialValues = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+  };
+
   return (
-    <View>
-      <TextInput
-        outlineColor={theme.colors.secondary}
-        style={styles.textInput}
-        label="Enter Username"
-        mode="outlined"
-        value={username}
-        onChangeText={(username) => setUsername(username)}
-      />
-      <TextInput
-        outlineColor={theme.colors.secondary}
-        style={{ ...styles.textInput, marginTop: 10 }}
-        label="Set Password"
-        mode="outlined"
-        value={password}
-        secureTextEntry={hidePass ? true : false}
-        right={
-          <TextInput.Icon icon="eye" onPress={() => setHidePass(!hidePass)} />
-        }
-        onChangeText={(password) => setPassword(password)}
-      />
-      <TextInput
-        outlineColor={theme.colors.secondary}
-        style={{ ...styles.textInput, marginTop: 10 }}
-        label="Confirm Password"
-        mode="outlined"
-        value={confirmPassword}
-        secureTextEntry={hideConfirmPass ? true : false}
-        right={
-          <TextInput.Icon
-            icon="eye"
-            onPress={() => setHideConfirmPass(!hideConfirmPass)}
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => console.debug(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+        <View>
+          <TextInput
+            outlineColor={theme.colors.secondary}
+            style={styles.textInput}
+            label="Enter Username"
+            mode="outlined"
+            onBlur={handleBlur("username")}
+            value={values.username}
+            onChangeText={handleChange("username")}
           />
-        }
-        onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
-      />
-      <Button
-        style={{ ...styles.buttonStyle, marginTop: 15 }}
-        buttonColor="#2E98DB"
-        icon="account-multiple-plus"
-        mode="contained"
-        onPress={() => console.debug("Pressed")}
-      >
-        Register
-      </Button>
-      <Button
-        style={{ ...styles.buttonStyle, marginTop: 15 }}
-        icon="login"
-        mode="outline"
-        onPress={() => loginHandler()}
-      >
-        Already have an account
-      </Button>
-    </View>
+          <TextInput
+            outlineColor={theme.colors.secondary}
+            style={{ ...styles.textInput, marginTop: 10 }}
+            label="Set Password"
+            mode="outlined"
+            value={values.password}
+            onBlur={handleBlur("password")}
+            secureTextEntry={hidePass ? true : false}
+            right={
+              <TextInput.Icon
+                icon="eye"
+                onPress={() => setHidePass(!hidePass)}
+              />
+            }
+            onChangeText={handleChange("password")}
+          />
+          <TextInput
+            outlineColor={theme.colors.secondary}
+            style={{ ...styles.textInput, marginTop: 10 }}
+            label="Confirm Password"
+            mode="outlined"
+            value={values.confirmPassword}
+            onBlur={handleBlur("confirmPassword")}
+            secureTextEntry={hideConfirmPass ? true : false}
+            right={
+              <TextInput.Icon
+                icon="eye"
+                onPress={() => setHideConfirmPass(!hideConfirmPass)}
+              />
+            }
+            onChangeText={handleChange("confirmPassword")}
+          />
+          <Button
+            style={{ ...styles.buttonStyle, marginTop: 15 }}
+            buttonColor="#2E98DB"
+            icon="account-multiple-plus"
+            mode="contained"
+            onPress={handleSubmit}
+          >
+            Register
+          </Button>
+          <Button
+            style={{ ...styles.buttonStyle, marginTop: 15 }}
+            icon="login"
+            mode="outline"
+            onPress={() => loginHandler()}
+          >
+            Already have an account
+          </Button>
+        </View>
+      )}
+    </Formik>
   );
 };
 
