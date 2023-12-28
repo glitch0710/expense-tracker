b77191e36563e8324eafaa6f36bf751a4e518984
Rhogiel Andoy <theglitch0710@gmail.com>
2023-12-28 12:27:08 +0800

    #25 fixed fetching of data

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,58 +1,22 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
-import {
-  doc,
-  getDocs,
-  onSnapshot,
-  collection,
-  query,
-} from "firebase/firestore";
+import { onSnapshot, collection } from "firebase/firestore";
 import { fireAppDb } from "../../firebase";
 
 const Expenses = () => {
-  //   const unsub = onSnapshot(doc(fireAppDb, "expenses", "8YxIdi1pxpDWOcvsHi7v"), (doc) => {
-  //     console.log("Current Data: ", doc.data());
-  //   });
   const [expenses, setExpenses] = useState([]);
-  const unsub = onSnapshot(collection(fireAppDb, "expenses"), (snapshot) => {
-    const rawExp = expenses;
-    snapshot.docChanges().forEach((change) => {
-      const expData = {
-        id: change.doc.id,
-        expense: change.doc.data().expense_name,
-        amount: change.doc.data().expense_amount,
-        date: change.doc.data().expense_date,
-      };
-      rawExp.push(expData);
+  useEffect(() => {
+    onSnapshot(collection(fireAppDb, "expenses"), (snapshot) => {
+      setExpenses(snapshot.docs.map((doc) => doc.data()));
     });
-    setExpenses(rawExp);
-  });
-  // const fetchExpenses = async () => {
-  //   const querySnapshot = await getDocs(collection(fireAppDb, "expenses"));
-  //   const data = querySnapshot.docs.map((doc) => ({
-  //     id: doc.id,
-  //     data: doc.data(),
-  //   }));
-
-  //   setExpenses(data);
-  // };
-
-  // useEffect(() => {
-  //   fetchExpenses();
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("rerendered")df
-  // }, [expenses]);
+  }, []);
 
   return (
     <View>
       <Text>Expenses</Text>
-      {/* <FlatList
-        data={expenses}
-        renderItem={({ item }) => <Text style={{color: "white"}}>{item.expense}</Text>}
-        keyExtractor={(item) => item.id}
-      /> */}
+      {expenses.map((exp) => {
+        return <Text style={{ color: "white" }}>{exp.expense_name}</Text>;
+      })}
     </View>
   );
 };
